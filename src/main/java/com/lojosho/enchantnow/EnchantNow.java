7fcee173529b9cd422a67eaca784e43fee03c120
LoJoSho <LoJoShogaming@gmail.com>
2022-07-12 13:07:44 -0500

    note

--- src/main/java/com/lojosho/enchantnow/EnchantNow.java
+++ src/main/java/com/lojosho/enchantnow/EnchantNow.java
@@ -17,6 +17,9 @@ public final class EnchantNow extends JavaPlugin {
     private static boolean hasEcoEnchants = false;
     private static EnchantNow instance;
 
+    /**
+     * To those who are viewing this code... well, it ain't the best but it could be better with your help! <3
+     */
     @Override
     public void onEnable() {
         // Plugin startup logic
